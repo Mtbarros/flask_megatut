498a6b7e194342a50c31c1e00c0a8a85785b9ade
Miguel Barros <miguel.barros@gmv.com>
2019-08-10 17:09:18 +0100

    fixes unnecessary environment change and adds another example route

--- megatut_app/routes.py
+++ megatut_app/routes.py
@@ -3,4 +3,8 @@ from megatut_app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return 'Hello, World1!'
+
+@app.route('/ola')
+def ola():
+    return 'OLA!'
