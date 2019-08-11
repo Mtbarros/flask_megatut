c03664bbc1d10063b5a4dd06ce747e16a943408c
Miguel Barros <miguel.barros@gmv.com>
2019-08-11 19:44:57 +0100

    introduces templating with chapter 2

--- megatut_app/routes.py
+++ megatut_app/routes.py
@@ -1,10 +1,18 @@
+from flask import render_template
 from megatut_app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World1!'
-
-@app.route('/ola')
-def ola():
-    return 'OLA!'
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
